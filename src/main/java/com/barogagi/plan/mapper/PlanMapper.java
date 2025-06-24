007a845ec830c5fa4fa439047ca519423386f6f3
RohDamin <rohdamin3@gmail.com>
2025-06-24 21:54:28 +0900

    feat: Plan 도메인 기본 controller, service, mapper, vo 생성

---
+++ src/main/java/com/barogagi/plan/mapper/PlanMapper.java
@@ -0,0 +1,7 @@
+package com.barogagi.plan.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PlanMapper {
+}
---
+++ src/main/java/com/barogagi/plan/service/PlanService.java
@@ -0,0 +1,7 @@
+package com.barogagi.plan.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlanService {
+}
