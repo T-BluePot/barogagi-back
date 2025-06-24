46f39661f347404bb0e1d94b1d28953e675fa477
RohDamin <rohdamin3@gmail.com>
2025-06-24 21:54:28 +0900

    feat: Plan 도메인 기본 controller, service, mapper, vo 생성

---
+++ src/main/java/com/barogagi/plan/controller/PlanController.java
@@ -0,0 +1,7 @@
+package com.barogagi.plan.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PlanController {
+}
---
+++ src/main/java/com/barogagi/plan/vo/PlanVO.java
@@ -0,0 +1,19 @@
+package com.barogagi.plan.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class PlanVO {
+    private int planNum;            // 계획 번호 (PK)
+    private int categoryNum;        // 카테고리 번호 (FK)
+    private String planNm;          // 계획명
+    private String startTime;       // 시작시간
+    private String endTime;         // 종료시간
+    private int regionNum;          // 지역 번호 (FK)
+    private int scheduleNum;        // 일정 번호 (FK)
+    private int membershipNo;       // 회원 번호 (FK)
+}
