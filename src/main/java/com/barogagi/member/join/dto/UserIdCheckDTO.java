64c1176bf82a9c3960ec00efc3b55dafba4df3e6
dksgyrud1349 <119853577+dksgyrud1349@users.noreply.github.com>
2025-07-20 17:52:04 +0900

    [FEAT] 닉네임 관련 작업 (#3)
    * 연결 테스트
    
    * Reapply .gitignore
    
    * style:일반로그인, 약관 VO -> DTO 로 변경
    
    * style: DefailtVO -> DefaultVO 로 이름 변경. 아이디 찾기에서 아이디를 리스트로 담아오는 로직에서 VO->DTO로 변경 및 일부 피드백 참고하여 수정
    
    * style: 사용되지 않는 import문 삭제
    
    * feat: 각자 application.properties 파일을 구성하거나 개발서버에 application.properties를 업데이트 시 암호화에 사용해주시면 좋을 것 같습니다.
    
    * style: dto 수정 및 쿼리 일부 수정
    
    * feat:회원가입 시 닉네임 저장 추가, 닉네임 중복 검증 API, 아이디, 비밀번호, 닉네임 규칙 추가, input validation 적용
    
    * style:주석 내용 수정
    
    * fix: feat/join-membership - Update DTOs for join membership
    
    ---------
    
    Co-authored-by: 정은우 <136785138+Jung-eunwoo@users.noreply.github.com>

---
+++ src/main/java/com/barogagi/member/join/dto/UserIdCheckDTO.java
@@ -0,0 +1,15 @@
+package com.barogagi.member.join.dto;
+
+import com.barogagi.config.vo.DefaultVO;
+import lombok.Getter;
+import lombok.Setter;
+import javax.validation.constraints.*;
+
+@Getter
+@Setter
+public class UserIdCheckDTO extends DefaultVO {
+    @NotBlank(message = "사용자 ID는 필수 입력값입니다.")
+    @Size(min = 4, max = 20, message = "사용자 ID는 4자 이상 20자 이하여야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "사용자 ID는 영문과 숫자만 사용 가능합니다.")
+    private String userId = "";
+}
