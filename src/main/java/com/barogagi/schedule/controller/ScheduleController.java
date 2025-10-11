512dd54532b932e79f5ad8e3984b9818cd7f8c46
RohDamin <rohdamin3@gmail.com>
2025-10-11 14:26:26 +0900

    update: 일정 상세 조회 API의 태그 조회 쿼리 수정

--- src/main/java/com/barogagi/schedule/controller/ScheduleController.java
+++ src/main/java/com/barogagi/schedule/controller/ScheduleController.java
@@ -60,54 +60,22 @@ public class ScheduleController {
     public ApiResponse getScheduleDetail(@Parameter(description = "조회할 일정 번호", example = "1")
                                          @RequestParam Integer scheduleNum) {
 
+
         logger.info("CALL /schedule/detail");
-        logger.info("[input] SchedulNm={}", scheduleNum);
+        logger.info("[input] scheduleNum={}", scheduleNum);
 
-        ApiResponse apiResponse = new ApiResponse();
-        String resultCode = "";
-        String message = "";
-
+        ScheduleDetailResDTO result;
         try {
-
+            // TODO. 해당 사용자의 일정이 맞는지도 체크해야 함
             //if(userIdCheckVO.getApiSecretKey().equals(API_SECRET_KEY)){
-            if (true) {
+            result = scheduleQueryService.getScheduleDetail(scheduleNum);
 
-                // TODO. 해당 사용자의 일정이 맞는지도 체크해야 함
-                if (inputValidate.isInvalidInteger(scheduleNum)) {
-                    resultCode = "101";
-                    message = "조회할 일정을 선택해주세요.";
-                } else {
-
-                    ScheduleDetailResDTO result = scheduleQueryService.getScheduleDetail(scheduleNum);
-                    logger.info("result={}", result.toString());
-
-                    if (result == null) {
-                        resultCode = "300";
-                        message = "조회할 일정이 존재하지 않습니다."; // TODO. 에러 메시지 정의하기
-
-                    } else {
-                        resultCode = "200";
-                        message = "일정 상세 조회 성공";
-                        apiResponse.setData(result);
-                        logger.info("#$# result={}", result.toString());
-                    }
-                }
-
-            } else {
-                resultCode = "100";
-                message = "잘못된 접근입니다.";
-            }
-            logger.info("#$# 11 resultCode={}", resultCode);
         } catch (Exception e) {
-            resultCode = "400";
-            message = "오류가 발생하였습니다.";
-            throw new RuntimeException(e);
-
-        } finally {
-            apiResponse.setResultCode(resultCode);
-            apiResponse.setMessage(message);
+            return ApiResponse.error("404", "일정 조회 실패");
         }
-        return apiResponse;
+
+
+        return ApiResponse.success(result, "일정 조회 성공");
     }
 
 
