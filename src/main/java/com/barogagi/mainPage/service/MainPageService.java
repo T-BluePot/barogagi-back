aaf4c1b9ee8b1955b728476382ce281f7d45cbe4
dksgyrud1349 <119853577+dksgyrud1349@users.noreply.github.com>
2025-11-25 13:04:52 +0900

    [FEAT] 메인 화면에 필요한 API 작업 (#15)
    * FEAT : 유저 일정 정보 조회 API 작업
    
    * FEAT : 인기 태그 조회 API 작업
    
    * FEAT : 인기 지역 조회 API 작업
    
    * UPDATE
    1. 회원 번호 검증 코드 수정
    2. 메인 화면 - 다가오는 일정 조회 API 에서 불필요한 데이터 조회하지 않도록 수정

---
+++ src/main/java/com/barogagi/mainPage/service/MainPageService.java
@@ -0,0 +1,48 @@
+package com.barogagi.mainPage.service;
+
+import com.barogagi.mainPage.dto.*;
+import com.barogagi.mainPage.mapper.MainPageMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MainPageService {
+
+    private static final Logger logger = LoggerFactory.getLogger(MainPageService.class);
+
+    private final MainPageMapper mainPageMapper;
+
+    @Autowired
+    public MainPageService(MainPageMapper mainPageMapper) {
+        this.mainPageMapper = mainPageMapper;
+    }
+
+    // 유저 일정 조회
+    public UserInfoResponseDTO selectUserScheduleInfo(UserInfoRequestDTO userInfoRequestDTO) throws Exception {
+        return mainPageMapper.selectUserScheduleInfo(userInfoRequestDTO);
+    }
+
+    // 해당 schedule에 대한 태그 목록 조회
+    public List<TagInfoDTO> selectScheduleTag(UserInfoRequestDTO userInfoRequestDTO) throws Exception {
+        return  mainPageMapper.selectScheduleTag(userInfoRequestDTO);
+    }
+
+    // 해당 plan에 대한 region 정보 조회
+    public RegionInfoDTO selectScheduleRegionInfo(UserInfoRequestDTO userInfoRequestDTO) throws Exception {
+        return mainPageMapper.selectScheduleRegionInfo(userInfoRequestDTO);
+    }
+
+    // 인기 지역 조회
+    public List<RegionRankInfoDTO> selectRegionRankList() throws Exception {
+        return mainPageMapper.selectRegionRankList();
+    }
+
+    // 인기 태그 조회
+    public List<TagRankInfoDTO> selectTagRankList() throws Exception {
+        return mainPageMapper.selectTagRankList();
+    }
+}
