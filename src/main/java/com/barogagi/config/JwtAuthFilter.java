233eeced835f4549b428c2f408e90ed102e4413d
안효경 <dksgyrud1349@naver.com>
2025-12-20 09:55:47 +0900

    [FEAT] : 로그 공통화 적용 작업 진행중

--- src/main/java/com/barogagi/config/JwtAuthFilter.java
+++ src/main/java/com/barogagi/config/JwtAuthFilter.java
@@ -2,15 +2,13 @@ package com.barogagi.config;
 
 import com.barogagi.member.info.dto.Member;
 import com.barogagi.member.info.service.MemberService;
-import com.barogagi.member.login.repository.UserMembershipRepository;
+import com.barogagi.member.login.exception.InvalidRefreshTokenException;
 import com.barogagi.util.JwtUtil;
 import com.barogagi.config.resultCode.ResultCode;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.JwtException;
@@ -22,15 +20,11 @@ import java.io.IOException;
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 
-    Logger logger = LoggerFactory.getLogger(JwtAuthFilter.class);
-
     private final JwtUtil jwt;
-    private final UserMembershipRepository userRepo;
     private final MemberService memberService;
 
-    public JwtAuthFilter(JwtUtil jwt, UserMembershipRepository userRepo, MemberService memberService) {
+    public JwtAuthFilter(JwtUtil jwt, MemberService memberService) {
         this.jwt = jwt;
-        this.userRepo = userRepo;
         this.memberService = memberService;
     }
 
@@ -60,20 +54,17 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         } catch (ExpiredJwtException e) {
             // 유효기간이 지나서 만료된 경우
             writeErrorResponse(
-                    res,
                     ResultCode.EXPIRE_TOKEN.getResultCode(),
                     ResultCode.EXPIRE_TOKEN.getMessage()
             );
         } catch (JwtException | SecurityException e) {
             // 위조되었거나 변조되었거나 구조가 잘못되었을 경우
             writeErrorResponse(
-                    res,
                     ResultCode.NOT_EXIST_ACCESS_AUTH.getResultCode(),
                     ResultCode.NOT_EXIST_ACCESS_AUTH.getMessage()
             );
         } catch (Exception e) {
             writeErrorResponse(
-                    res,
                     ResultCode.NOT_EXIST_ACCESS_AUTH.getResultCode(),
                     ResultCode.NOT_EXIST_ACCESS_AUTH.getMessage()
             );
@@ -87,16 +78,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         return p.startsWith("/auth/") || p.startsWith("/login/basic/membership/userId/search");
     }
 
-    private void writeErrorResponse(HttpServletResponse res, String resultCode, String message) throws IOException {
-        res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        res.setContentType("application/json;charset=UTF-8");
-
-        String json = String.format(
-                "{\"resultCode\":\"%s\", \"message\":\"%s\"}",
-                resultCode, message
-        );
-
-        res.getWriter().write(json);
+    private void writeErrorResponse(String resultCode, String message) throws IOException {
+        throw new InvalidRefreshTokenException(resultCode, message);
     }
 
 }
--- src/main/java/com/barogagi/member/info/service/MemberService.java
+++ src/main/java/com/barogagi/member/info/service/MemberService.java
@@ -39,128 +39,101 @@ public class MemberService {
 
     public ApiResponse selectMemberInfo(HttpServletRequest request) {
 
-        String resultCode = "";
-        String message = "";
-        Member data = null;
+        // 1. 회원번호 구하기
+        Map<String, Object> membershipNoInfo = membershipUtil.membershipNoService(request);
+        if(!membershipNoInfo.get("resultCode").equals("200")) {
+            throw new MemberInfoException(
+                    String.valueOf(membershipNoInfo.get("resultCode")),
+                    String.valueOf(membershipNoInfo.get("message"))
+            );
+        }
+        String membershipNo = String.valueOf(membershipNoInfo.get("membershipNo"));
 
-        try {
-
-            // 1. 회원번호 구하기
-            Map<String, Object> membershipNoInfo = membershipUtil.membershipNoService(request);
-            if(!membershipNoInfo.get("resultCode").equals("200")) {
-                throw new MemberInfoException(
-                        String.valueOf(membershipNoInfo.get("resultCode")),
-                        String.valueOf(membershipNoInfo.get("message"))
-                );
-            }
-            String membershipNo = String.valueOf(membershipNoInfo.get("membershipNo"));
-
-            // 2. 회원 정보 조회
-            Member memberInfo = this.findByMembershipNo(membershipNo);
-            if(null == memberInfo) {
-                throw new MemberInfoException(
-                        ProcessResultCode.NOT_FOUND_USER_INFO.getResultCode(),
-                        ProcessResultCode.NOT_FOUND_USER_INFO.getMessage()
-                );
-            }
-
-            // 이메일 복호화
-            memberInfo.setEmail(encryptUtil.decrypt(memberInfo.getEmail()));
-
-            // 전화번호 복호화
-            memberInfo.setTel(encryptUtil.decrypt(memberInfo.getTel()));
-
-            // 비밀번호는 보내주지 않는다
-            memberInfo.setPassword("");
-
-            resultCode = ProcessResultCode.FOUND_USER_INFO.getResultCode();
-            message = ProcessResultCode.FOUND_USER_INFO.getMessage();
-            data = memberInfo;
-
-        } catch (MemberInfoException ex) {
-            resultCode = ex.getResultCode();
-            message = ex.getMessage();
-        } catch (Exception e) {
-            resultCode = ResultCode.ERROR.getResultCode();
-            message = ResultCode.ERROR.getMessage();
+        // 2. 회원 정보 조회
+        Member memberInfo = this.findByMembershipNo(membershipNo);
+        if(null == memberInfo) {
+            throw new MemberInfoException(
+                    ProcessResultCode.NOT_FOUND_USER_INFO.getResultCode(),
+                    ProcessResultCode.NOT_FOUND_USER_INFO.getMessage()
+            );
         }
 
-        return ApiResponse.resultData(data, resultCode, message);
+        // 이메일 복호화
+        memberInfo.setEmail(encryptUtil.decrypt(memberInfo.getEmail()));
+
+        // 전화번호 복호화
+        memberInfo.setTel(encryptUtil.decrypt(memberInfo.getTel()));
+
+        // 비밀번호는 보내주지 않는다
+        memberInfo.setPassword("");
+
+        return ApiResponse.resultData(
+                memberInfo,
+                ProcessResultCode.FOUND_USER_INFO.getResultCode(),
+                ProcessResultCode.FOUND_USER_INFO.getMessage()
+        );
     }
 
     public ApiResponse updateMemberProcess(HttpServletRequest request, MemberRequestDTO memberRequestDTO) {
 
-        String resultCode = "";
-        String message = "";
-
-        try {
-
-            // 1. 회원번호 구하기
-            Map<String, Object> membershipNoInfo = membershipUtil.membershipNoService(request);
-            if(!membershipNoInfo.get("resultCode").equals("200")) {
-                throw new MemberInfoException(
-                        String.valueOf(membershipNoInfo.get("resultCode")),
-                        String.valueOf(membershipNoInfo.get("message")));
-            }
-
-            String membershipNo = String.valueOf(membershipNoInfo.get("membershipNo"));
-
-            // 2. 회원 정보 조회
-            Member memberInfo = this.findByMembershipNo(membershipNo);
-            if(null == memberInfo) {
-                throw new MemberInfoException(
-                        ProcessResultCode.NOT_FOUND_USER_INFO.getResultCode(),
-                        ProcessResultCode.NOT_FOUND_USER_INFO.getMessage()
-                );
-            }
-
-            // 3. 데이터 처리
-            // 생년월일
-            if(!memberRequestDTO.getBirth().isEmpty()) {
-                memberInfo.setBirth(memberRequestDTO.getBirth().replaceAll("[^0-9]", ""));
-            }
-
-            // 성별 (M : 남 / W : 여)
-            if(!memberRequestDTO.getGender().isEmpty()) {
-                memberInfo.setGender(memberRequestDTO.getGender());
-            }
-
-            // 닉네임(중복X)
-            if(!memberRequestDTO.getNickName().isEmpty()) {
-                NickNameDTO nickNameRequest = new NickNameDTO();
-                nickNameRequest.setNickName(memberRequestDTO.getNickName());
-                int nickNameCnt = joinService.selectNicknameCnt(nickNameRequest);
-
-                if(nickNameCnt > 0) {
-                    throw new MemberInfoException(
-                            ProcessResultCode.UNAVAILABLE_NICKNAME.getResultCode(),
-                            ProcessResultCode.UNAVAILABLE_NICKNAME.getMessage()
-                    );
-                }
-
-                memberInfo.setNickName(memberRequestDTO.getNickName());
-            }
-
-            int updateMemberInfo = this.updateMemberInfo(memberInfo);
-            if(updateMemberInfo <= 0) {
-                throw new MemberInfoException(
-                        ProcessResultCode.FAIL_UPDATE_USER_INFO.getResultCode(),
-                        ProcessResultCode.FAIL_UPDATE_USER_INFO.getMessage()
-                );
-            }
-
-            resultCode = ProcessResultCode.SUCCESS_UPDATE_USER_INFO.getResultCode();
-            message = ProcessResultCode.SUCCESS_UPDATE_USER_INFO.getMessage();
-
-        } catch (MemberInfoException ex) {
-            resultCode = ex.getResultCode();
-            message = ex.getMessage();
-        } catch (Exception e) {
-            resultCode = ResultCode.ERROR.getResultCode();
-            message = ResultCode.ERROR.getMessage();
+        // 1. 회원번호 구하기
+        Map<String, Object> membershipNoInfo = membershipUtil.membershipNoService(request);
+        if(!membershipNoInfo.get("resultCode").equals("200")) {
+            throw new MemberInfoException(
+                    String.valueOf(membershipNoInfo.get("resultCode")),
+                    String.valueOf(membershipNoInfo.get("message")));
         }
 
-        return ApiResponse.result(resultCode, message);
+        String membershipNo = String.valueOf(membershipNoInfo.get("membershipNo"));
+
+        // 2. 회원 정보 조회
+        Member memberInfo = this.findByMembershipNo(membershipNo);
+        if(null == memberInfo) {
+            throw new MemberInfoException(
+                    ProcessResultCode.NOT_FOUND_USER_INFO.getResultCode(),
+                    ProcessResultCode.NOT_FOUND_USER_INFO.getMessage()
+            );
+        }
+
+        // 3. 데이터 처리
+        // 생년월일
+        if(!memberRequestDTO.getBirth().isEmpty()) {
+            memberInfo.setBirth(memberRequestDTO.getBirth().replaceAll("[^0-9]", ""));
+        }
+
+        // 성별 (M : 남 / W : 여)
+        if(!memberRequestDTO.getGender().isEmpty()) {
+            memberInfo.setGender(memberRequestDTO.getGender());
+        }
+
+        // 닉네임(중복X)
+        if(!memberRequestDTO.getNickName().isEmpty()) {
+            NickNameDTO nickNameRequest = new NickNameDTO();
+            nickNameRequest.setNickName(memberRequestDTO.getNickName());
+            int nickNameCnt = joinService.selectNicknameCnt(nickNameRequest);
+
+            if(nickNameCnt > 0) {
+                throw new MemberInfoException(
+                        ProcessResultCode.UNAVAILABLE_NICKNAME.getResultCode(),
+                        ProcessResultCode.UNAVAILABLE_NICKNAME.getMessage()
+                );
+            }
+
+            memberInfo.setNickName(memberRequestDTO.getNickName());
+        }
+
+        int updateMemberInfo = this.updateMemberInfo(memberInfo);
+        if(updateMemberInfo <= 0) {
+            throw new MemberInfoException(
+                    ProcessResultCode.FAIL_UPDATE_USER_INFO.getResultCode(),
+                    ProcessResultCode.FAIL_UPDATE_USER_INFO.getMessage()
+            );
+        }
+
+        return ApiResponse.result(
+                ProcessResultCode.SUCCESS_UPDATE_USER_INFO.getResultCode(),
+                ProcessResultCode.SUCCESS_UPDATE_USER_INFO.getMessage()
+        );
     }
 
     public Member findByMembershipNo(String membershipNo) {
