bae0a5f6a6739655ed09ffcf257c3ce679e25ce4
안효경 <dksgyrud1349@naver.com>
2025-12-29 19:29:00 +0900

    [FIX] : 에러 코드 형식 변경

--- src/main/java/com/barogagi/member/basic/join/service/BasicJoinService.java
+++ src/main/java/com/barogagi/member/basic/join/service/BasicJoinService.java
@@ -1,20 +1,18 @@
 package com.barogagi.member.basic.join.service;
 
 import com.barogagi.config.PasswordConfig;
-import com.barogagi.config.resultCode.ProcessResultCode;
 import com.barogagi.member.basic.join.dto.JoinDTO;
 import com.barogagi.member.basic.join.dto.JoinRequestDTO;
 import com.barogagi.member.basic.join.dto.NickNameDTO;
 import com.barogagi.member.basic.join.exception.JoinException;
 import com.barogagi.member.login.dto.RefreshTokenRequestDTO;
-import com.barogagi.member.login.exception.InvalidRefreshTokenException;
 import com.barogagi.member.login.service.AccountService;
 import com.barogagi.member.login.service.AuthService;
 import com.barogagi.response.ApiResponse;
 import com.barogagi.util.EncryptUtil;
 import com.barogagi.util.InputValidate;
-import com.barogagi.config.resultCode.ResultCode;
 import com.barogagi.util.Validator;
+import com.barogagi.util.exception.ErrorCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -54,26 +52,17 @@ public class BasicJoinService {
 
         // 1. API SECRET KEY 일치 여부 확인
         if(!validator.apiSecretKeyCheck(apiSecretKey)) {
-            throw new JoinException(
-                    ResultCode.NOT_EQUAL_API_SECRET_KEY.getResultCode(),
-                    ResultCode.NOT_EQUAL_API_SECRET_KEY.getMessage()
-            );
+            throw new JoinException(ErrorCode.NOT_EQUAL_API_SECRET_KEY);
         }
 
         // 2. 필수 입력값 확인
         if(inputValidate.isEmpty(nickname)) {
-            throw new JoinException(
-                    ProcessResultCode.EMPTY_DATA.getResultCode(),
-                    ProcessResultCode.EMPTY_DATA.getMessage()
-            );
+            throw new JoinException(ErrorCode.EMPTY_DATA);
         }
 
         // 3. 적합한 닉네임인지 확인
         if(!validator.isValidNickname(nickname)) {
-            throw new JoinException(
-                    ProcessResultCode.INVALID_NICKNAME.getResultCode(),
-                    ProcessResultCode.INVALID_NICKNAME.getMessage()
-            );
+            throw new JoinException(ErrorCode.INVALID_NICKNAME);
         }
 
         // 4. 닉네임 중복 체크
@@ -82,16 +71,10 @@ public class BasicJoinService {
 
         int nickNameCnt = joinService.selectNicknameCnt(nickNameDTO);
         if(nickNameCnt > 0) {
-            throw new JoinException(
-                    ProcessResultCode.UNAVAILABLE_NICKNAME.getResultCode(),
-                    ProcessResultCode.UNAVAILABLE_NICKNAME.getMessage()
-            );
+            throw new JoinException(ErrorCode.UNAVAILABLE_NICKNAME);
         }
 
-        return ApiResponse.result(
-                ProcessResultCode.AVAILABLE_NICKNAME.getResultCode(),
-                ProcessResultCode.AVAILABLE_NICKNAME.getMessage()
-        );
+        return ApiResponse.result(ErrorCode.AVAILABLE_NICKNAME);
     }
 
     // 아이디 중복 체크 service
@@ -99,26 +82,17 @@ public class BasicJoinService {
 
         // 1. API SECRET KEY 일치 여부 확인
         if(!validator.apiSecretKeyCheck(apiSecretKey)) {
-            throw new JoinException(
-                    ResultCode.NOT_EQUAL_API_SECRET_KEY.getResultCode(),
-                    ResultCode.NOT_EQUAL_API_SECRET_KEY.getMessage()
-            );
+            throw new JoinException(ErrorCode.NOT_EQUAL_API_SECRET_KEY);
         }
 
         // 2. 필수 입력값 확인
         if(inputValidate.isEmpty(userId)) {
-            throw new JoinException(
-                    ProcessResultCode.EMPTY_DATA.getResultCode(),
-                    ProcessResultCode.EMPTY_DATA.getMessage()
-            );
+            throw new JoinException(ErrorCode.EMPTY_DATA);
         }
 
         // 3. 적합한 아이디인지 확인
         if(!validator.isValidId(userId)) {
-            throw new JoinException(
-                    ProcessResultCode.INVALID_USER_ID.getResultCode(),
-                    ProcessResultCode.INVALID_USER_ID.getMessage()
-            );
+            throw new JoinException(ErrorCode.INVALID_USER_ID);
         }
 
         // 4. 아이디 중복 체크
@@ -128,26 +102,17 @@ public class BasicJoinService {
         int checkUserId = joinService.selectUserIdCnt(joinDTO);
 
         if(checkUserId > 0) {
-            throw new JoinException(
-                    ProcessResultCode.UNAVAILABLE_USER_ID.getResultCode(),
-                    ProcessResultCode.UNAVAILABLE_USER_ID.getMessage()
-            );
+            throw new JoinException(ErrorCode.UNAVAILABLE_USER_ID);
         }
 
-        return ApiResponse.result(
-                ProcessResultCode.AVAILABLE_USER_ID.getResultCode(),
-                ProcessResultCode.AVAILABLE_USER_ID.getMessage()
-        );
+        return ApiResponse.result(ErrorCode.AVAILABLE_USER_ID);
     }
 
     public ApiResponse signUp(JoinRequestDTO joinRequestDTO) {
 
         // 1. API SECRET KEY 일치 여부 확인
         if(!validator.apiSecretKeyCheck(joinRequestDTO.getApiSecretKey())) {
-            throw new JoinException(
-                    ResultCode.NOT_EQUAL_API_SECRET_KEY.getResultCode(),
-                    ResultCode.NOT_EQUAL_API_SECRET_KEY.getMessage()
-            );
+            throw new JoinException(ErrorCode.NOT_EQUAL_API_SECRET_KEY);
         }
 
         // 2. 필수 입력값 확인
@@ -157,10 +122,7 @@ public class BasicJoinService {
                 || inputValidate.isEmpty(joinRequestDTO.getPassword())
                 || inputValidate.isEmpty(joinRequestDTO.getTel()))
         {
-            throw new JoinException(
-                    ProcessResultCode.EMPTY_DATA.getResultCode(),
-                    ProcessResultCode.EMPTY_DATA.getMessage()
-            );
+            throw new JoinException(ErrorCode.EMPTY_DATA);
         }
 
         // 3. 적합한 아이디인지 확인
@@ -169,10 +131,7 @@ public class BasicJoinService {
                 validator.isValidId(joinRequestDTO.getUserId())
                         && validator.isValidPassword(joinRequestDTO.getPassword()))
         ) {
-            throw new JoinException(
-                    ProcessResultCode.INVALID_SIGN_UP.getResultCode(),
-                    ProcessResultCode.INVALID_SIGN_UP.getMessage()
-            );
+            throw new JoinException(ErrorCode.INVALID_SIGN_UP);
         }
 
         // 4. 암호화
@@ -205,10 +164,7 @@ public class BasicJoinService {
 
         // 5. 아이디 중복 검증
         if(duplicateUserId > 0) {
-            throw new JoinException(
-                    ProcessResultCode.UNAVAILABLE_USER_ID.getResultCode(),
-                    ProcessResultCode.UNAVAILABLE_USER_ID.getMessage()
-            );
+            throw new JoinException(ErrorCode.UNAVAILABLE_USER_ID);
         }
 
         // 닉네임 값이 넘어올 경우 중복 검사
@@ -216,10 +172,7 @@ public class BasicJoinService {
 
             // 닉네임 적합성 검사
             if(!validator.isValidNickname(joinRequestDTO.getNickName())) {
-                throw new JoinException(
-                        ProcessResultCode.INVALID_NICKNAME.getResultCode(),
-                        ProcessResultCode.INVALID_NICKNAME.getMessage()
-                );
+                throw new JoinException(ErrorCode.INVALID_NICKNAME);
             }
 
             NickNameDTO nickNameDTO = new NickNameDTO();
@@ -227,41 +180,31 @@ public class BasicJoinService {
             int selectNicknameCnt = joinService.selectNicknameCnt(nickNameDTO);
 
             if(selectNicknameCnt > 0) {
-                throw new JoinException(
-                        ProcessResultCode.UNAVAILABLE_NICKNAME.getResultCode(),
-                        ProcessResultCode.UNAVAILABLE_NICKNAME.getMessage()
-                );
+                throw new JoinException(ErrorCode.UNAVAILABLE_NICKNAME);
             }
         }
 
         // 6. 회원 정보 저장
         int insertResult = joinService.insertMembershipInfo(joinDTO);
         if(insertResult <= 0){
-            throw new JoinException(
-                    ProcessResultCode.FAIL_SIGN_UP.getResultCode(),
-                    ProcessResultCode.FAIL_SIGN_UP.getMessage()
-            );
+            throw new JoinException(ErrorCode.FAIL_SIGN_UP);
         }
 
-        return ApiResponse.result(
-                ProcessResultCode.SUCCESS_SIGN_UP.getResultCode(),
-                ProcessResultCode.SUCCESS_SIGN_UP.getMessage()
-        );
+        return ApiResponse.result(ErrorCode.SUCCESS_SIGN_UP);
     }
 
     public ApiResponse deleteAccount(RefreshTokenRequestDTO refreshTokenRequestDTO) {
 
         // 1. refresh token이 공백 또는 null인지 확인
         if(inputValidate.isEmpty(refreshTokenRequestDTO.getRefreshToken())) {
-            throw new JoinException(
-                    ProcessResultCode.EMPTY_DATA.getResultCode(),
-                    ProcessResultCode.EMPTY_DATA.getMessage());
+            throw new JoinException(ErrorCode.EMPTY_DATA);
         }
 
         // 2. refresh token을 이용해서 membershipNo 구하기
         Map<String, String> resultMap = authService.selectUserInfoByToken(refreshTokenRequestDTO.getRefreshToken());
         if(!resultMap.get("resultCode").equals("200")) {
-            throw new InvalidRefreshTokenException(
+
+            return ApiResponse.error(
                     resultMap.get("resultCode"),
                     resultMap.get("message")
             );
@@ -269,16 +212,10 @@ public class BasicJoinService {
 
         int deleteResult = accountService.deleteMyAccount(resultMap.get("membershipNo"));
         if(deleteResult <= 0) {
-            throw new JoinException(
-                    ProcessResultCode.FAIL_DELETE_ACCOUNT.getResultCode(),
-                    ProcessResultCode.FAIL_DELETE_ACCOUNT.getMessage()
-            );
+            throw new JoinException(ErrorCode.FAIL_DELETE_ACCOUNT);
         }
 
-        return ApiResponse.result(
-                ProcessResultCode.SUCCESS_DELETE_ACCOUNT.getResultCode(),
-                ProcessResultCode.SUCCESS_DELETE_ACCOUNT.getMessage()
-        );
+        return ApiResponse.result(ErrorCode.SUCCESS_DELETE_ACCOUNT);
     }
 }
 
--- src/main/java/com/barogagi/util/exception/BasicException.java
+++ src/main/java/com/barogagi/util/exception/BasicException.java
@@ -6,20 +6,11 @@ import lombok.Getter;
 @Getter
 public class BasicException extends BusinessException {
 
-    private final String resultCode;
     private final ErrorCode errorCode;
 
     // 신규 생성자
     public BasicException(ErrorCode errorCode) {
-        super(errorCode.getCode(), errorCode.getMessage());
+        super(errorCode);
         this.errorCode = errorCode;
-        this.resultCode = errorCode.getCode();
-    }
-
-    // 기존 생성자
-    public BasicException(String resultCode, String message) {
-        super(resultCode, message);
-        this.resultCode = resultCode;
-        errorCode = null;
     }
 }
