3a646019a93c3331621bb421b0563c3d2249bc0a
Hyokyung An <119853577+dksgyrud1349@users.noreply.github.com>
2026-01-24 15:54:24 +0900

    [FEAT/FIX] 예외 발생 시 디스코드 알람 발송, 로그아웃 코드 결과값 수정 (#83)
    * [FIX] : 로그아웃 코드 수정 (#81)
    
    * [FIX] : 로그아웃 시 결과 수정
    
    * [FEAT] : 에러 발생 시 디스코드 메신저 발송 기능 추가 (#82)
    
    * [FEAT] : 에러 발생 시 디스코드 메신저 발송 기능 추가

---
+++ src/main/java/com/barogagi/config/AsyncConfig.java
@@ -0,0 +1,10 @@
+package com.barogagi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
+
