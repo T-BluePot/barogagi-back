cc30340fb03fa2a25474fd3be1d54308d46b3c0c
RohDamin <rohdamin3@gmail.com>
2026-02-19 21:27:55 +0900

    feat: 아이템(세부 카테고리) 목록 조회 API 작성

--- src/main/java/com/barogagi/schedule/command/service/ScheduleCommandService.java
+++ src/main/java/com/barogagi/schedule/command/service/ScheduleCommandService.java
@@ -20,7 +20,7 @@ import com.barogagi.plan.dto.PlanRegistResDTO;
 import com.barogagi.plan.dto.UserAddedPlaceDTO;
 import com.barogagi.plan.enums.PLAN_SOURCE;
 import com.barogagi.category.query.mapper.CategoryMapper;
-import com.barogagi.plan.query.mapper.ItemMapper;
+import com.barogagi.item.query.mapper.ItemMapper;
 import com.barogagi.region.command.entity.Place;
 import com.barogagi.region.command.entity.PlanRegion;
 import com.barogagi.region.command.entity.PlanRegionId;
