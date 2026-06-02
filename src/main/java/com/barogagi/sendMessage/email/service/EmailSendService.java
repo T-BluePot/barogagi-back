96a8a1d974fee803f79f643c894eca349dd5a438
안효경 <dksgyrud1349@naver.com>
2026-06-02 14:45:40 +0900

    [fix] : 실서버에서만 문자, 알림톡, 이메일 발송 가능 처리

--- src/main/java/com/barogagi/sendMessage/email/service/EmailSendService.java
+++ src/main/java/com/barogagi/sendMessage/email/service/EmailSendService.java
@@ -1,12 +1,14 @@
 package com.barogagi.sendMessage.email.service;
 
 import com.barogagi.batch.dto.SendDTO;
+import com.barogagi.sendMessage.service.CommonService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
@@ -17,21 +19,26 @@ public class EmailSendService {
 
     private final AsyncSmtpMailService asyncSmtpMailService;
     private final SpringTemplateEngine templateEngine;
+    private final CommonService commonService;
 
     public boolean sendWithdrawlEmail(SendDTO sendDTO) {
 
         try {
             sendDTO.getSendMailDTO().setBody(buildWithdrawlHtml(sendDTO.getVariables()));
 
-            // 실제 메일 발송
-            CompletableFuture<Boolean> futureResult = asyncSmtpMailService.sendMailAsync(sendDTO.getSendMailDTO());
+            if(commonService.isProd()) {  // 실서버에서만 이메일 발송
+                // 실제 메일 발송
+                CompletableFuture<Boolean> futureResult = asyncSmtpMailService.sendMailAsync(sendDTO.getSendMailDTO());
 
-            // 발송 결과를 기다림
-            boolean result = futureResult.get(); // 필요 시 timeout 설정 가능
-            if (!result) {
-                log.error("메일 발송 실패: {}", sendDTO.getSendMailDTO().getTo());
+                // 발송 결과를 기다림
+                boolean result = futureResult.get(); // 필요 시 timeout 설정 가능
+                if (!result) {
+                    log.error("메일 발송 실패: {}", sendDTO.getSendMailDTO().getTo());
+                }
+                return result;
+            } else {  // 개발/테스트 서버에서는 항상 성공 처리
+                return true;
             }
-            return result;
 
         } catch (Exception e) {
             log.error("이메일 발송 실패", e);
