72ec0e7345dad0f887da5e7e740e4a12afedbdae
안효경 <dksgyrud1349@naver.com>
2026-06-04 17:18:02 +0900

    [FIX] : 푸쉬 호출 전 알림 동의 여부 확인

--- src/main/java/com/barogagi/setting/mapper/SettingMapper.java
+++ src/main/java/com/barogagi/setting/mapper/SettingMapper.java
@@ -10,4 +10,7 @@ public interface SettingMapper {
 
     // 설정 목록 조회 (회원 상세페이지에서 조회)
     List<MemberSettingDTO> selectSettings(String membershipNo);
+
+    // 알림 수신 설정 ON 여부
+    MemberSettingDTO selectMemberSettingValue(MemberSettingDTO memberSettingDTO);
 }
