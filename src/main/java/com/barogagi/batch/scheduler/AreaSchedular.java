8a5acc9f66f29c1ae323128ad63dc48b4399525e
안효경 <dksgyrud1349@naver.com>
2026-09-19 00:26:00 +0900

    FIX : 배치 주기 수정

--- src/main/java/com/barogagi/batch/scheduler/AreaSchedular.java
+++ src/main/java/com/barogagi/batch/scheduler/AreaSchedular.java
@@ -29,13 +29,17 @@ public class AreaSchedular {
     지자체별 타 관광지와 가장 많이 연결되는 중심 관광지 100위 정보를 제공합니다.
      */
     // 전국·지역별 추천, 인기 지역
-    @Scheduled(cron = "0 20 0 * * *")
+    @Scheduled(cron = "0 30 3 1 * *")
     @SchedulerLock(
             name = "localPopularAreaBatch",
             lockAtMostFor = "30m",
             lockAtLeastFor = "5m"
     )
     public void localPopularAreaBatch() {
-        publicDataService.insertLocalPopularArea();
+        log.info("flag={}", commonService.isProd());
+        log.info("serverType={}", (Object) environment.getActiveProfiles());
+        if(commonService.isProd()) {
+            publicDataService.insertLocalPopularArea();
+        }
     }
 }
